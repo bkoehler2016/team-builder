907907e09077636215803fa02ed810a79d42c11b
Ben Koehler <bkoehler85282@gmail.com>
2019-12-10 13:48:06 -0700

    added files back

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Members from "./components/Members";
+import teamMemberForm from "./components/teamMemberForm";
 import "./App.css";
 
 function App() {
+  const [team, setTeam] = useState([
+    { name: "Ben", role: "Student", email: "ben@lambda.com" },
+    { name: "Adam", role: "Dog", email: "adam@lambda.com" },
+  ]);
   return (
-    <div>
-      <h1>My New React Form!</h1>
+    <div className="App">
+      <h1>Team Builder Form></h1>
+      <teamMemberForm setTeam={setTeam} />
+      {team.map(person => (
+        <Members key={person.name} person={person} />
+      ))}
     </div>
   );
 }
