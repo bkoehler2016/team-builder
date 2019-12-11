e040cc5e74a9af759902c71dd0ea60a7c5bab3b9
Ben Koehler <bkoehler85282@gmail.com>
2019-12-10 19:37:39 -0700

    updated styling

--- src/components/Members.js
+++ src/components/Members.js
@@ -4,10 +4,12 @@ const Members = props => {
   const person = props.person;
 
   return (
-    <div className="Member">
-      <h2>{person.name}</h2>
-      <h3>{person.role}</h3>
-      <p>{person.email}</p>
+    <div className="Member-list">
+      <div className="Member">
+        <h2>{person.name}</h2>
+        <h3>{person.role}</h3>
+        <p>{person.email}</p>
+      </div>
     </div>
   );
 };
